c48b83db16de9693f4edaa60e76c345ede85d018
sofiamejiamuro <sofiamejiamuro@gmail.com>
2019-11-12 23:15:49 -0600

    definicion y maquetado de proyectos, seccion de contacto

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ const menuAppear = () => {
     document.getElementById("navbarList").style.display = "flex"; 
 }
 
+const changeNav  = () => {
+    document.getElementById("projects").style.display = "none"; 
+    document.getElementById("contact").style.display = "block"; 
+}
+
+
+const changeNav2  = () => {
+    document.getElementById("contact").style.display = "none"; 
+    document.getElementById("projects").style.display = "block"; 
+}
