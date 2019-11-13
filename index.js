327682b5262c076630f7004f93f91ae64c7b0a38
sofiamejiamuro <sofiamejiamuro@gmail.com>
2019-11-12 18:54:24 -0600

    initializing portfolio repo

---
+++ index.js
@@ -0,0 +1,6 @@
+const menuAppear = () => {
+    console.log('menu');
+    document.getElementById("arrow").style.display = "none"; 
+    document.getElementById("navbarList").style.display = "flex"; 
+}
+
